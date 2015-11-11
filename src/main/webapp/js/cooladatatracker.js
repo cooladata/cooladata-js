cf7842f73a96bea1eeacd7b1b12480e2fda53054
yuval <yuval.levav@cooladata.com>
2015-11-11 17:50:18 +0200

    update maven plugin moved to version 2.1.12

--- src/main/webapp/js/cooladatatracker.js
+++ src/main/webapp/js/cooladatatracker.js
@@ -45,7 +45,7 @@
      */
     var HTTP_PROTOCOL = (("https:" == document.location.protocol) ? "https://" : "http://"),
 
-        LIB_VERSION = '2.1.12',
+        LIB_VERSION = '2.1.13',
         SNIPPET_VERSION = (cooladata && cooladata['__SV']) || 0,
 
     // http://hacks.mozilla.org/2009/07/cross-site-xmlhttprequest-with-cors/
