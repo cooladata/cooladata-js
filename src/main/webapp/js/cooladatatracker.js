251ec99bb27a2b4a3724c4cec7401c2fc3f047ee
Ariel <ariel@cooladata.com>
2017-05-08 14:43:28 +0300

    change version

--- src/main/webapp/js/cooladatatracker.js
+++ src/main/webapp/js/cooladatatracker.js
@@ -45,7 +45,7 @@
      */
     var HTTP_PROTOCOL = (("http:" == document.location.protocol) ? "http://" : "https://"),
 
-        LIB_VERSION = '2.1.17',
+        LIB_VERSION = '2.1.18',
         SNIPPET_VERSION = (cooladata && cooladata['__SV']) || 0,
 
     // http://hacks.mozilla.org/2009/07/cross-site-xmlhttprequest-with-cors/
